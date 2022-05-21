d4b6c9376a8386802b67dcaa5bc01e9baadebddd
jo763 <joe.prollins@hotmail.com>
2022-05-21 19:45:55 +0100

    added fast attackers not fin

--- Scenes/Bullet.gd
+++ Scenes/Bullet.gd
@@ -6,6 +6,6 @@ func _physics_process(delta):
 	position -= transform.y * speed * delta
 
 func _on_Bullet_body_entered(body):
-	if body.is_in_group("mobs"):
+	if body.is_in_group("enemy"):
 		body.queue_free()
 	queue_free()
